9c0fcdbdfe7edb702eef3e1260e5761a71de7041
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-07-15 19:35:03 +0500

    Add estimate Fuse gas in SendModal

--- hooks/useEstimateGas.ts
+++ hooks/useEstimateGas.ts
@@ -3,15 +3,19 @@ import { publicClient } from "@/lib/wagmi";
 import { useEffect, useState } from "react";
 import { mainnet } from "viem/chains";
 
-export const useEstimateGas = (gasEstimate: bigint = 700000n) => {
+export const useEstimateGas = (
+  gasEstimate: bigint = 700000n,
+  chainId: number = mainnet.id,
+  token: string = "ETH"
+) => {
   const [costInUsd, setCostInUsd] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const estimateGas = async () => {
       setLoading(true);
-      const gasPrice = await publicClient(mainnet.id).getGasPrice();
-      const tokenPriceUsd = await fetchTokenPriceUsd("ETH");
+      const gasPrice = await publicClient(chainId).getGasPrice();
+      const tokenPriceUsd = await fetchTokenPriceUsd(token);
       const costInUsd = gasEstimate * gasPrice;
       setCostInUsd((Number(costInUsd) * Number(tokenPriceUsd)) / 10 ** 18);
       setLoading(false);
