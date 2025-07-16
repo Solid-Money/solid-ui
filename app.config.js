a711894e1b18d07923d83829c7b90f39401b1ed4
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-07-16 19:43:17 +0500

    Revert slug in app config

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   expo: {
     name: "Solid",
-    slug: "Solid",
+    slug: "flash-frontend",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
