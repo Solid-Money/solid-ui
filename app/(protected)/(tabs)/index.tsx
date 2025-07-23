49dcda9ebd70c09225bc68ca32eb107d72cd8e4b
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-07-23 19:56:26 +0500

    Reduce margin right in SavingCountUp

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -160,7 +160,7 @@ export default function Dashboard() {
                             fontSize: isScreenMedium ? 96 : 48,
                             fontWeight: isScreenMedium ? "medium" : "semibold",
                             color: "#ffffff",
-                            marginRight: -5,
+                            marginRight: -2,
                           },
                           decimalText: {
                             fontSize: isScreenMedium ? 40 : 24,
