c6affdbadb2c06b3f41906a412f7a15707472473
Samet Sahin <a.sahin.ual@gmail.com>
2025-08-07 16:56:09 +0300

    remove the useNav hook as it's not necessary

---
+++ constants/menu-items.ts
@@ -0,0 +1,18 @@
+import { path } from '@/constants/path';
+import { Href } from 'expo-router';
+
+export type MenuItem = {
+  label: string;
+  href: Href;
+};
+
+export const menuItems: MenuItem[] = [
+  {
+    label: 'Home',
+    href: path.HOME,
+  },
+  {
+    label: 'Savings',
+    href: path.SAVINGS,
+  },
+];
