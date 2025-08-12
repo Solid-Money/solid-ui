bf87bf97db395f29fe81ec6fc73bd7a0f96a656f
Samet Sahin <a.sahin.ual@gmail.com>
2025-08-13 01:21:58 +0300

    Update path constants to include new routes and standardize quotes
    - Added `USER_KYC_INFO` and `BANK_TRANSFER` routes to the `Path` type.
    - Updated the existing `CARD_KYC` route to `KYC` for clarity.
    - Standardized string quotes from double to single throughout the `path` object for consistency.

--- constants/path.ts
+++ constants/path.ts
@@ -1,4 +1,4 @@
-import { Href, Route } from "expo-router";
+import { Href, Route } from 'expo-router';
 
 type Path = {
   REGISTER: Href;
@@ -11,41 +11,43 @@ type Path = {
   SEND: Href;
   SWAP: Href;
   CARD: Href;
+  USER_KYC_INFO: Href;
+  BANK_TRANSFER: Href;
   CARD_ACTIVATE: Route;
-  CARD_KYC: Route;
+  KYC: Href;
   CARD_TERMS_OF_SERVICE: Route;
   CARD_DETAILS: Route;
   CARD_ACTIVATE_MOBILE: Href;
-  CARD_USER_INFO_MOBILE: Route;
   CARD_KYC_MOBILE: Href;
   EARN: Href;
   BUY_CRYPTO: Href;
   SETTINGS: Href;
   NOTIFICATIONS: Href;
   PASSKEY_NOT_SUPPORTED: Href;
-}
+};
 
 export const path: Path = {
-  REGISTER: "/register",
-  WELCOME: "/welcome",
-  HOME: "/",
-  SAVINGS: "/savings",
-  ACTIVITY: "/activity",
-  WALLET: "/wallet",
-  DEPOSIT: "/deposit",
-  SEND: "/send",
-  SWAP: "/swap",
-  CARD: "/card",
-  CARD_ACTIVATE: "/card/activate",
-  CARD_KYC: "/card/kyc",
-  CARD_TERMS_OF_SERVICE: "/card/bridge_terms_of_service",
-  CARD_DETAILS: "/card/details",
-  CARD_ACTIVATE_MOBILE: "/card/activate_mobile",
-  CARD_USER_INFO_MOBILE: "/card/user_info_mobile",
-  CARD_KYC_MOBILE: "/card/kyc_mobile",
-  EARN: "/earn",
-  BUY_CRYPTO: "/buy-crypto",
-  SETTINGS: "/settings",
-  NOTIFICATIONS: "/notifications",
-  PASSKEY_NOT_SUPPORTED: "/passkey-not-supported",
-}
+  REGISTER: '/register',
+  WELCOME: '/welcome',
+  HOME: '/',
+  SAVINGS: '/savings',
+  ACTIVITY: '/activity',
+  WALLET: '/wallet',
+  DEPOSIT: '/deposit',
+  SEND: '/send',
+  SWAP: '/swap',
+  CARD: '/card',
+  USER_KYC_INFO: '/user-kyc-info',
+  KYC: '/kyc',
+  BANK_TRANSFER: '/bank-transfer',
+  CARD_ACTIVATE: '/card/activate',
+  CARD_TERMS_OF_SERVICE: '/card/bridge_terms_of_service',
+  CARD_DETAILS: '/card/details',
+  CARD_ACTIVATE_MOBILE: '/card/activate_mobile',
+  CARD_KYC_MOBILE: '/card/kyc_mobile',
+  EARN: '/earn',
+  BUY_CRYPTO: '/buy-crypto',
+  SETTINGS: '/settings',
+  NOTIFICATIONS: '/notifications',
+  PASSKEY_NOT_SUPPORTED: '/passkey-not-supported',
+};
