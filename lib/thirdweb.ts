9117999d452e9d24fdf0337ae5defb4b3b260c84
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-08-14 17:58:32 +0500

    Add Fuse chain in Thirdweb

--- lib/thirdweb.ts
+++ lib/thirdweb.ts
@@ -1,4 +1,4 @@
-import { createThirdwebClient } from 'thirdweb';
+import { createThirdwebClient, defineChain } from 'thirdweb';
 import { arbitrum, base, mainnet, polygon } from 'thirdweb/chains';
 
 import { EXPO_PUBLIC_THIRDWEB_CLIENT_ID } from './config';
@@ -7,7 +7,23 @@ export const client = createThirdwebClient({
   clientId: EXPO_PUBLIC_THIRDWEB_CLIENT_ID,
 });
 
-const chains = [mainnet, polygon, base, arbitrum];
+const fuse = defineChain({
+  id: 122,
+  rpc: "https://rpc.fuse.io",
+  nativeCurrency: {
+    name: "Fuse",
+    symbol: "FUSE",
+    decimals: 18,
+  },
+  blockExplorers: [
+    { 
+      name: 'Blockscout',
+      url: 'https://explorer.fuse.io',
+    },
+  ]
+});
+
+const chains = [mainnet, fuse, polygon, base, arbitrum];
 
 export const getChain = (chainId: number) => {
   return chains.find(chain => chain.id === chainId);
