4c5f18eb4219fe419ee93c3eeee70f29b09f04ea
Samet Sahin <a.sahin.ual@gmail.com>
2025-08-14 17:39:09 +0300

    show bank transfer option in settings on native

--- constants/settings.ts
+++ constants/settings.ts
@@ -1,4 +1,5 @@
-import { Href } from "expo-router";
+import { Href } from 'expo-router';
+import { Platform } from 'react-native';
 
 interface Account {
   title: string;
@@ -13,28 +14,39 @@ interface Support {
 
 export const accounts: Account[] = [
   {
-    title: "Account details",
-    description: "Name & email",
-    link: "/settings/account",
+    title: 'Account details',
+    description: 'Name & email',
+    link: '/settings/account',
   },
   {
-    title: "Wallet recovery",
-    description: "Secure your account",
-    link: "/settings/account",
+    title: 'Wallet recovery',
+    description: 'Secure your account',
+    link: '/settings/account',
   },
 ];
 
-export const supports: Support[] = [
+const supports: Support[] = [
   {
-    title: "Help & Support",
-    link: "/settings/account",
+    title: 'Help & Support',
+    link: '/settings/account',
   },
   {
-    title: "Legal",
-    link: "/settings/account",
+    title: 'Legal',
+    link: '/settings/account',
   },
   {
-    title: "Rate us",
-    link: "/settings/account",
+    title: 'Rate us',
+    link: '/settings/account',
   },
 ];
+
+// The deposit options modal is not showing on native, so to have a way
+// to access the bank transfer page, we add it to the settings page
+if (Platform.OS !== 'web') {
+  supports.push({
+    title: 'Bank transfer',
+    link: '/bank-transfer',
+  });
+}
+
+export { supports };
