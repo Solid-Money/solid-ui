50cdacf18c7b7e52bfdb3e234a9b279b04ba5c40
Samet Sahin <a.sahin.ual@gmail.com>
2025-08-20 17:00:10 +0300

    design the user kyc info screen

---
+++ components/UserKyc/UserInfoHeader.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Text } from '@/components/ui/text';
+
+import { KycMode } from './types';
+
+interface UserInfoHeaderProps {
+  kycMode?: KycMode;
+}
+
+export function UserInfoHeader({ kycMode }: UserInfoHeaderProps) {
+  const text =
+    kycMode === KycMode.BANK_TRANSFER
+      ? 'We need some basic information to get started with your bank transfer'
+      : kycMode === KycMode.CARD
+        ? 'We need some basic information to get started with your card activation'
+        : 'We need some basic information to get started';
+
+  return <Text className="text-base text-[#ACACAC] text-center">{text}</Text>;
+}
