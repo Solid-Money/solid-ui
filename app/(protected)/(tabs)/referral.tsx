6ca910b0e2ab60f204d2c21b0c9d41751ba18744
Mayank Mittal <msquare.pvt21@gmail.com>
2025-08-25 22:55:53 +0530

    fix: ui fixes

--- app/(protected)/(tabs)/referral.tsx
+++ app/(protected)/(tabs)/referral.tsx
@@ -1,6 +1,5 @@
 import CopyToClipboard from '@/components/CopyToClipboard';
 import Navbar from '@/components/Navbar';
-import ReferralTitle from '@/components/Points/RewardsTitle';
 import { Text } from '@/components/ui/text';
 import { useDimension } from '@/hooks/useDimension';
 import useUser from '@/hooks/useUser';
@@ -22,34 +21,30 @@ export default function Savings() {
     >
       <ScrollView className="flex-1">
         {isScreenMedium && <Navbar />}
-        <View className="gap-8 md:gap-16 px-4 pt-4 pb-8 w-full max-w-7xl mx-auto">
-          {isScreenMedium ? (
-            <View className="flex-row justify-between items-center mt-5">
-              <ReferralTitle />
-            </View>
-          ) : (
-            <View className="flex-row items-center justify-between">
+        <View className="flex-1 justify-center gap-10 px-4 py-8 w-full max-w-lg mx-auto">
+          <View className="gap-10 md:gap-20 w-full mx-auto">
+            <View className="flex-row items-center justify-between md:mt-16">
               <Pressable onPress={() => router.back()} className="web:hover:opacity-70">
                 <ArrowLeft color="white" />
               </Pressable>
               <Text className="text-white text-xl md:text-3xl font-semibold text-center">
-                Share your referral code
+                Refer a Friend
               </Text>
               <View className="w-10" />
             </View>
-          )}
-          <View className="flex-col items-center justify-center w-full">
-            <Image
-              source={require('@/assets/images/referral_large.png')}
-              style={{ width: 250, height: 250 }}
-            />
-            <Text className="text-white/70 text-center mt-8">
-              Earn 10% of your referral&apos;s points when <br />
-              they use Solid App
-            </Text>
-            <View className="flex-row w-[400px] justify-between items-center p-4 ps-6 bg-primary/10 rounded-2xl text-primary font-medium mt-6">
-              <Text>https://app.solid.app/join/{user?.referralCode}</Text>
-              <CopyToClipboard text={`https://app.solid.app/join/${user?.referralCode}`} />
+            <View className="flex-col items-center justify-center w-full">
+              <Image
+                source={require('@/assets/images/referral_large.png')}
+                style={{ width: 250, height: 250 }}
+              />
+              <Text className="text-white/70 text-center mt-8">
+                Earn 10% of your referral&apos;s points when <br />
+                they use Solid App
+              </Text>
+              <View className="flex-row w-[400px] justify-between items-center p-4 ps-6 bg-primary/10 rounded-2xl text-primary font-medium mt-6">
+                <Text>https://app.solid.app/join/{user?.referralCode}</Text>
+                <CopyToClipboard text={`https://app.solid.app/join/${user?.referralCode}`} />
+              </View>
             </View>
           </View>
         </View>
