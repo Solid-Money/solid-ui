105d57f50e84c042b219a612f97812391cee6f81
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-08-31 14:00:12 +0500

    Invert background and foreground color in Tooltip

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -24,11 +24,11 @@ function TooltipContent({
           entering={Platform.select({ web: undefined, default: FadeIn })}
           exiting={Platform.select({ web: undefined, default: FadeOut })}
         >
-          <TextClassContext.Provider value="text-sm native:text-base text-popover-foreground">
+          <TextClassContext.Provider value="text-sm native:text-base text-primary-foreground">
             <TooltipPrimitive.Content
               sideOffset={sideOffset}
               className={cn(
-                'z-50 overflow-hidden rounded-md bg-popover px-3 py-1.5 shadow-md shadow-foreground/5 web:animate-in web:fade-in-0 web:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+                'z-50 overflow-hidden rounded-md bg-foreground px-3 py-1.5 shadow-md shadow-foreground/5 web:animate-in web:fade-in-0 web:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
                 className,
               )}
               {...props}
