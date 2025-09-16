1e33371fc80921c3a42cd8de3c2a64b2626c4d91
Mayank Mittal <msquare.pvt21@gmail.com>
2025-09-16 16:41:46 +0530

    fix: Check and update safe address on login

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -419,6 +419,20 @@ const useUser = (): UseUserReturn => {
 
       const smartAccountClient = await safeAA(mainnet, user.subOrganizationId, user.walletAddress);
 
+      if (!user.safeAddress || user.safeAddress == '') {
+        const resp = await withRefreshToken(() =>
+          updateSafeAddress(smartAccountClient.account.address),
+        );
+        if (!resp) {
+          const error = new Error('Error updating safe address on login');
+          Sentry.captureException(error, {
+            tags: {
+              type: 'safe_address_update_error',
+            },
+          });
+        }
+      }
+
       const selectedUser: User = {
         safeAddress: smartAccountClient.account.address,
         username: user.username,
