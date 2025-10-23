ae80f76833ed4d47d1d82ded7dcfa8ec1d1ef3eb
Samet Şahin <a.sahin.ual@gmail.com>
2025-10-23 11:22:00 +0300

    Remove deprecated DepositExternalWalletOptions component and clean up related code. Update DepositExternalWalletOptions.web.tsx to remove TODO comment for deposit flow implementation.

--- components/DepositOption/DepositExternalWalletOptions.tsx
+++
@@ -1,3 +0,0 @@
-// This file will be replaced by the platform-specific version at build time
-// Export web version by default (for development/testing)
-export { default } from './DepositExternalWalletOptions.web';
