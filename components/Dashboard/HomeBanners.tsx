cc5b5edf424749458708d5b323c389e0eb48d019
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-10-31 01:23:36 +0500

    Add two slide per view in banner carousel

--- components/Dashboard/HomeBanners.tsx
+++ components/Dashboard/HomeBanners.tsx
@@ -1,20 +1,15 @@
 import React, { useRef, useState } from 'react';
-import { Dimensions, LayoutChangeEvent, StyleSheet, View } from 'react-native';
+import { LayoutChangeEvent, StyleSheet, View } from 'react-native';
 import { useSharedValue } from 'react-native-reanimated';
 import Carousel, { ICarouselInstance, Pagination } from 'react-native-reanimated-carousel';
+import { CarouselRenderItemInfo } from 'react-native-reanimated-carousel/lib/typescript/types';
 
-import { useDimension } from '@/hooks/useDimension';
 import PoolStat from '@/components/Savings/PoolStat';
-import DepositBanner from './DepositBanner';
+import { useDimension } from '@/hooks/useDimension';
 import CardBanner from './CardBanner';
+import DepositBanner from './DepositBanner';
 
 type BannerData = React.ReactElement[];
-
-type BannerItem = {
-  item: BannerData[number];
-};
-
-const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 const data: BannerData = [
   <DepositBanner key="deposit" />,
@@ -22,15 +17,15 @@ const data: BannerData = [
   <PoolStat key="pool" />,
 ];
 
-// Won't be used until we have more than one banner
 export const HomeBanners = () => {
   const ref = useRef<ICarouselInstance>(null);
   const progress = useSharedValue<number>(0);
   const { isScreenMedium } = useDimension();
-  const [containerWidth, setContainerWidth] = useState(SCREEN_WIDTH - 32);
+  const [containerWidth, setContainerWidth] = useState(0);
 
   const GAP = 16;
   const ITEM_WIDTH = isScreenMedium ? (containerWidth - GAP) / 2 : containerWidth;
+  const VIEW_COUNT = isScreenMedium ? 2 : 1;
   const BANNER_HEIGHT = isScreenMedium ? 220 : 170;
 
   const onPressPagination = (index: number) => {
@@ -40,8 +35,13 @@ export const HomeBanners = () => {
     });
   };
 
-  const renderItem = ({ item }: BannerItem) => {
-    return <View className="flex-1 w-full h-full">{item}</View>;
+  const renderItem = ({ item, index }: CarouselRenderItemInfo<BannerData[number]>) => {
+    const isFirstItem = VIEW_COUNT > 1 && index === ref.current?.getCurrentIndex();
+    return (
+      <View className="flex-1 h-full" style={{ marginRight: isFirstItem ? GAP : 0 }}>
+        {item}
+      </View>
+    );
   };
 
   const onLayout = (event: LayoutChangeEvent) => {
@@ -51,31 +51,33 @@ export const HomeBanners = () => {
 
   return (
     <View style={styles.container} onLayout={onLayout}>
-      <Carousel
-        ref={ref}
-        width={ITEM_WIDTH}
-        height={BANNER_HEIGHT}
-        data={data}
-        loop={false}
-        autoPlay={false}
-        scrollAnimationDuration={1000}
-        windowSize={2}
-        customConfig={() => ({ type: 'positive', viewCount: 2 })}
-        onProgressChange={progress}
-        renderItem={renderItem}
-        style={{
-          width: '100%',
-        }}
-      />
-      {data.length > 1 && (
-        <Pagination.Custom
-          progress={progress}
-          data={data}
-          dotStyle={styles.dotStyle}
-          activeDotStyle={styles.activeDot}
-          containerStyle={styles.paginationContainer}
-          onPress={onPressPagination}
-        />
+      {containerWidth > 0 && (
+        <>
+          <Carousel
+            key={containerWidth}
+            ref={ref}
+            width={ITEM_WIDTH}
+            height={BANNER_HEIGHT}
+            data={data}
+            loop={true}
+            autoPlay={false}
+            onProgressChange={progress}
+            renderItem={renderItem}
+            style={{
+              width: containerWidth,
+            }}
+          />
+          {data.length > 1 && (
+            <Pagination.Custom
+              progress={progress}
+              data={data}
+              dotStyle={styles.dotStyle}
+              activeDotStyle={styles.activeDot}
+              containerStyle={styles.paginationContainer}
+              onPress={onPressPagination}
+            />
+          )}
+        </>
       )}
     </View>
   );
