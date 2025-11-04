d9ccc8f572fe8fe0f42a2fdfb794419421bc8eb6
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-11-04 18:33:07 +0500

    Add rounded corners on focus in account dropdown

--- components/AccountCenter/AccountCenterDropdown.tsx
+++ components/AccountCenter/AccountCenterDropdown.tsx
@@ -34,7 +34,7 @@ const AccountCenterDropdown = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger className="rounded-full">
         <AccountCenterTrigger />
       </DropdownMenuTrigger>
       <DropdownMenuContent
