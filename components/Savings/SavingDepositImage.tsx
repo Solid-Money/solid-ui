f4f44ef8ab324bafcd81146baeeebf97cb185151
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-11-04 18:19:12 +0500

    Reduce SavingDepositImage size on mobile

--- components/Savings/SavingDepositImage.tsx
+++ components/Savings/SavingDepositImage.tsx
@@ -1,14 +1,18 @@
 import { Image } from 'expo-image';
 
+import { useDimension } from '@/hooks/useDimension';
+
 const SavingDepositImage = () => {
+  const { isScreenMedium } = useDimension();
+  
   return (
     <Image
       source={require('@/assets/images/solid-purple-large.png')}
       contentFit="contain"
       style={{
-        width: 349,
-        height: 378,
-        marginTop: -30,
+        width: isScreenMedium ? 349 : 233,
+        height: isScreenMedium ? 378 : 252,
+        marginTop: isScreenMedium ? -30 : -15,
         marginBottom: -30,
         marginLeft: 'auto',
         marginRight: 'auto',
