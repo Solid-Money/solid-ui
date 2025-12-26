c8eaf010ac3bdcc930ba77a3b1b4df04967a3d0d
Samet Şahin <a.sahin.ual@gmail.com>
2025-12-26 14:20:06 +0300

    refactor: simplify PriceImpact component rendering and enhance dialog overlay styling
    - Streamlined PriceImpact component by removing unnecessary View wrapper.
    - Updated dialog overlay background opacity for improved visibility.

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -40,7 +40,7 @@ const DialogOverlayWeb = React.forwardRef<DialogPrimitive.OverlayRef, DialogPrim
     return (
       <DialogPrimitive.Overlay
         className={cn(
-          'web:backdrop-blur-[4px] bg-black/40 flex justify-center items-center p-2 absolute top-0 right-0 bottom-0 left-0',
+          'web:backdrop-blur-[4px] bg-black/80 flex justify-center items-center p-2 absolute top-0 right-0 bottom-0 left-0',
           className,
         )}
         onPointerDown={handlePointerDown}
