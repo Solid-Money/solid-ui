773635162ebbc852f5a49d6c4a94464d1048c5b0
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2026-01-11 18:16:57 +0500

    Fix SavingDepositButton not clickable

--- components/Savings/SavingDepositButton.tsx
+++ components/Savings/SavingDepositButton.tsx
@@ -2,23 +2,18 @@ import { View } from 'react-native';
 import { Plus } from 'lucide-react-native';
 
 import DepositOptionModal from '@/components/DepositOption/DepositOptionModal';
-import { buttonVariants } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
 
 const SavingDepositButton = () => {
   const getTrigger = () => {
     return (
-      <View
-        className={buttonVariants({
-          variant: 'indigo',
-          className: 'h-[3.375rem] rounded-xl pr-6',
-        })}
-      >
+      <Button variant="indigo" className="h-[3.375rem] rounded-xl pr-6">
         <View className="flex-row items-center gap-2">
           <Plus color="white" />
-          <Text className="text-lg font-bold">Make your first deposit</Text>
+          <Text className="text-lg font-bold text-foreground">Make your first deposit</Text>
         </View>
-      </View>
+      </Button>
     );
   };
 
