9c01ca18b6736a8622d7fe55782faca305338c9e
Lior Ageniho <Lior.Agnin@gmail.com>
2026-04-03 11:34:01 +0300

    Merge pull request #1828 from Solid-Money/app-version-1.0.6
    chore: bump app version to 1.0.6

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Solid',
   slug: 'flash-frontend',
-  version: '1.0.5',
+  version: '1.0.6',
   orientation: 'portrait',
   icon: './assets/images/adaptive-icon.png',
   scheme: 'solid',
