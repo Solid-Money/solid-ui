eb54704d3d8981a3eaf474188d9e41891963df14
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2026-06-25 16:45:24 +0500

    Bump version from 1.0.9 to 1.0.10

--- app.config.ts
+++ app.config.ts
@@ -51,7 +51,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Solid',
   slug: 'flash-frontend',
-  version: '1.0.9',
+  version: '1.0.10',
   orientation: 'portrait',
   icon: './assets/images/adaptive-icon.png',
   scheme: 'solid',
